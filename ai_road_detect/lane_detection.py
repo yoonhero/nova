7ed64b3153e0df14ec2fa22d2edee664dd8b7b8f
yoonhero <yoonhero06@naver.com>
2021-10-08 20:09:22 +0900

    feat: lane detection restart #4

--- ai_road_detect/lane_detection.py
+++ ai_road_detect/lane_detection.py
@@ -3,6 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def make_coordinate(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1*(3/5))
+    x1 = int((y1-intercept)/slope)
+    x2 = int((y2-intercept)/slope)
+    return np.array([x1, y1, x2, y2])
+
+
 def average_slope_intercept(image, lines):
     left_fit = []
     right_fit = []
@@ -17,7 +26,9 @@ def average_slope_intercept(image, lines):
             right_fit.append((slope, intercept))
     left_fit_average = np.average(left_fit, axis=0)
     right_fit_average = np.average(right_fit, axis=0)
-    print(left_fit_average, right_fit_average)
+    left_line = make_coordinate(image, left_fit_average)
+    right_line = make_coordinate(image, right_fit_average)
+    return np.array([left_line, right_line])
 
 
 def canny(image):
@@ -30,8 +41,7 @@ def canny(image):
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
+        for x1, y1, x2, y2 in lines:
             cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
     return line_image
 
@@ -41,27 +51,31 @@ def region_of_interest(image):
     width = image.shape[1]
     right_point = width // 10
     left_point = width // 10 * 8
-
     polygons = np.array(
-        [[(right_point, height), (left_point, height), (height//5*4, width//2)]])
+        [[(right_point, height), (left_point, height), (width//2, height//2)]])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons, 255)
     masked_image = cv2.bitwise_and(image, mask)
     return masked_image
 
 
-image = cv2.imread("../test_img/road.jpg")
+image = cv2.imread("road_sample.jpg")
 lane_image = np.copy(image)
+
+
 canny = canny(lane_image)
+
 cropped_image = region_of_interest(canny)
+
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi / 180,
                         100, np.array([]), minLineLength=40, maxLineGap=5)
-average_image = average_slope_intercept(lane_image, lines)
-line_image = display_lines(lane_image, lines)
-combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-# plt.imshow(canny)
-# plt.show()
 
-cv2.imshow("canny", combo_image)
+average_image = average_slope_intercept(lane_image, lines)
+line_image = display_lines(lane_image, average_image)
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+
+cv2.imshow("canny image", canny)
+cv2.imshow("cropped iamge", cropped_image)
+cv2.imshow("result", combo_image)
 
 cv2.waitKey(0)
