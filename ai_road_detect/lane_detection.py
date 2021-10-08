e2fa9cc083798408040ebbe17d4f558b5b90b82a
yoonhero <yoonhero06@naver.com>
2021-10-08 16:55:23 +0900

    feat: lane detection restart #3

--- ai_road_detect/lane_detection.py
+++ ai_road_detect/lane_detection.py
@@ -3,11 +3,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    print(left_fit_average, right_fit_average)
+
+
 def canny(image):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
     canny = cv2.Canny(blur, 50, 150)
     return canny
+
+
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    return line_image
 
 
 def region_of_interest(image):
@@ -20,16 +46,22 @@ def region_of_interest(image):
         [[(right_point, height), (left_point, height), (height//5*4, width//2)]])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons, 255)
-    return mask
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
 
 
 image = cv2.imread("../test_img/road.jpg")
 lane_image = np.copy(image)
 canny = canny(lane_image)
-
+cropped_image = region_of_interest(canny)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi / 180,
+                        100, np.array([]), minLineLength=40, maxLineGap=5)
+average_image = average_slope_intercept(lane_image, lines)
+line_image = display_lines(lane_image, lines)
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
 # plt.imshow(canny)
 # plt.show()
 
-cv2.imshow("canny", region_of_interest(canny))
+cv2.imshow("canny", combo_image)
 
 cv2.waitKey(0)
