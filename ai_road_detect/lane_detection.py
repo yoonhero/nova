d1acffa765455e8cb4a2b343873ed8d20808d0ba
yoonhero <yoonhero06@naver.com>
2021-10-08 16:29:04 +0900

    feat: lane detection restart #2

--- ai_road_detect/lane_detection.py
+++ ai_road_detect/lane_detection.py
@@ -1,14 +1,35 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+def canny(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    canny = cv2.Canny(blur, 50, 150)
+    return canny
+
+
+def region_of_interest(image):
+    height = image.shape[0]
+    width = image.shape[1]
+    right_point = width // 10
+    left_point = width // 10 * 8
+
+    polygons = np.array(
+        [[(right_point, height), (left_point, height), (height//5*4, width//2)]])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+    return mask
+
 
 image = cv2.imread("../test_img/road.jpg")
 lane_image = np.copy(image)
-gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-blur = cv2.GaussianBlur(gray, (5, 5), 0)
-canny = cv2.Canny(gray, 50, 150)
+canny = canny(lane_image)
 
-cv2.imshow("gray", gray)
-cv2.imshow("blur", blur)
-cv2.imshow("canny", canny)
+# plt.imshow(canny)
+# plt.show()
+
+cv2.imshow("canny", region_of_interest(canny))
 
 cv2.waitKey(0)
