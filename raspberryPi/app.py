99d3ba9b651b79585bddeb3f8b1ce34c83a3f299
yoonhero <yoonhero06@naver.com>
2022-01-15 23:01:15 +0900

    feat: Python Socket Io Simple Practice

--- raspberryPi/app.py
+++ raspberryPi/app.py
@@ -1,24 +1,48 @@
-from flask import Flask, url_for, render_template
-from socketio import Server, WSGIApp
-
-# Create Flask Application
-app = Flask(
-    __name__,
-    static_url_path="/static/",
-    static_folder=STATIC_FOLDER,
-    template_folder=TEMPLATE_FOLDER
-)
-
-# Create SocketIO Server
-sio = Server(
-    async_mode="threading",
-    logger=app.logger,
-    engineio_logger=app.logger
-)
-
-# Set SocketIO WSGI Application
-app.wsgi_app = WSGIApp(sio, app.wsgi_app)
+from flask import Flask, render_template, session, copy_current_request_context
+from flask_socketio import SocketIO, emit, disconnect
+from threading import Lock
 
 
-if __name__ == "__main__":
-    app.run(host="localhost", port=3000, threaded=True)
+async_mode = None
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+
+socket_ = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', async_mode=socket_.async_mode)
+
+
+@socket_.on('my_event', namespace='/test')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 10
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']})
+
+
+@socket_.on('my_broadcast_event', namespace='/test')
+def test_broadcast_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']},
+         broadcast=True)
+
+
+@socket_.on('disconnect_request', namespace='/test')
+def disconnect_request():
+    @copy_current_request_context
+    def can_disconnect():
+        disconnect()
+
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'Disconnected!', 'count': session['receive_count']},
+         callback=can_disconnect)
+
+
+if __name__ == '__main__':
+    socket_.run(app, debug=True)
