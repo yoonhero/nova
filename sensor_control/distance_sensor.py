93b13af8c77f3b96bfbba1ed56461564ddbb95b1
yoonhero <yoonhero06@naver.com>
2022-01-16 12:35:12 +0900

    refactor: Distance Sensor Object

--- sensor_control/distance_sensor.py
+++ sensor_control/distance_sensor.py
@@ -1,50 +1,54 @@
 import RPi.GPIO as GPIO
 import time
 
-# GPIO Mode (BOARD / BCM)
-GPIO.setmode(GPIO.BCM)
 
-# set Distance Pins
-Distance_TRIGGER = 18
-Distance_ECHO = 24
+class DistanceSensor():
+    def __init__(self):
+        GPIO.cleanup()
+        # GPIO Mode (BOARD / BCM)
+        GPIO.setmode(GPIO.BCM)
 
-# set GPIO direction (IN / OUT)
-GPIO.setup(Distance_TRIGGER, GPIO.OUT)
-GPIO.setup(Distance_ECHO, GPIO.IN)
+        # set Distance Pins
+        self.triggerPin = 18
+        self.echoPin = 24
 
+        # set GPIO direction (IN / OUT)
+        GPIO.setup(self.triggerPin, GPIO.OUT)
+        GPIO.setup(self.echoPin, GPIO.IN)
 
-def distance():
-    # set Trigger to HIGH
-    GPIO.output(Distance_TRIGGER, True)
+    def getDistance(self):
+        # set Trigger to HIGH
+        GPIO.output(self.triggerPin, True)
 
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(Distance_TRIGGER, False)
+        # set Trigger after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(self.triggerPin, False)
 
-    StartTime = time.time()
-    StopTime = time.time()
-
-    # save StartTime
-    while GPIO.input(Distance_ECHO) == 0:
         StartTime = time.time()
-
-    # save time of arrival
-    while GPIO.input(Distance_ECHO) == 1:
         StopTime = time.time()
 
-    # time difference between start and arrival
-    timeIntervel = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (timeIntervel * 34300) / 2
+        # save StartTime
+        while GPIO.input(self.echoPin) == 0:
+            StartTime = time.time()
 
-    return distance
+        # save time of arrival
+        while GPIO.input(self.echoPin) == 1:
+            StopTime = time.time()
+
+        # time difference between start and arrival
+        timeIntervel = StopTime - StartTime
+        # multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance = (timeIntervel * 34300) / 2
+
+        return distance
 
 
 if __name__ == '__main__':
     try:
         while True:
-            dist = distance()
+            sensor = DistanceSensor()
+            dist = sensor.getDistance()
             print("Measured Distance = %.1f cm" % dist)
             time.sleep(1)
 
