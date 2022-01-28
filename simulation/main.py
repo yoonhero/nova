8ab2d64a7ff38bfa07046deebe80aa227c1507ac
unknown <yoonhero06@naver.com>
2022-01-28 15:02:41 +0900

    feat: Simulation Camera Management

--- simulation/main.py
+++ simulation/main.py
@@ -325,40 +325,162 @@ class KeyboardControl(object):
                     world.restart()
                 
 
+######################## CAMERA MANAGEMENT #########################
+class CameraManager(object):
+    """Class for camera management"""
 
+    def ___init__(self, parent_actor, hud):
+        self.sensor = None
+        self.surface = None
+        self._parent = parent_actor
+        self.hud = hud
+        self.recording = False
+        
+        bound_y = 0.5 + self._parent.bounding_box.extent.y
+        attachment = carla.AttachmentType
+        self._camera_transforms = [
+            (carla.Transform(
+                carla.Location(x=-5.5, z=2.5), carla.Rotation(pitch=8.0)
+            ), attachment.SpringArm),
+            (carla.Transform(
+                carla.Location(x=1.6, z=1.7)
+            ), attachment.Rigid),
+            (carla.Transform(carla.Location(x=-8.0, y=1.5, z=1.5)), attachment.SpringArm),
+            (carla.Transform(carla.Location(x=-8.0, z=6.0), carla.Rotation(pitch=6.0)), attachment.SpringArm),
+            (carla.Transform(carla.Location(x=-1, y=-bound_y, z=0.5)), attachment.Rigid)
+        ]
+        
+        self.transform_index = 1
+        self.sensors = [
+            ['sensor.camera.rgb', cc.Raw, 'Camera RGB'],
+            ['sensor.camera.depth', cc.Depth, 'Camera Depth (Gray Scale)']
+        ]
+        
+        world = self._parent.get_world()
+        bp_library = world.get_blueprint_library()
+        for item in self.sensors:
+            blp = bp_library.find(item[0])
+            if item[0].startswith('sensor.camera'):
+                blp.set_attribute("image_size_x", str(hud.dim[0]))
+                blp.set_attribute("image_size_y", str(hud.dim[1]))
+            elif item[0].startswith('sensor.lidar'):
+                blp.set_attribute('range', '50')
+            item.append(blp)
+        self.index = None
+    
+    def toggle_camera(self):
+        """Activate a camera"""
+        self.transform_index = (self.transform_index + 1) % len(self._camera_transforms)
+        self.set_sensor(self.index, notify=False, force_respawn=True)
 
+    def set_sensor(self, index, notify=True, force_respawn=False):
+        """Set a sensor"""
+        index = index % len(self.sensors)
+
+        needs_respawn = True if self.index is None else (
+            force_respawn or (self.sensors[index][0] != self.sensors[self.index][0])
+        )
+        
+        if needs_respawn:
+            if self.sensor is not None:
+                self.sensor.destroy()
+                self.surface = None
+            self.sensor = self._parent.get_world().spawn_actor(
+                self.sensors[index][-1],
+                self._camera_transforms[self.transform_index][0],
+                attach_to=self._parent,
+                attachment_type=self._camera_transforms[self.transform_index][1]
+            )
+            
+            weak_self = weakref.ref(self)
+            self.sensor.listen(lambda image: CameraManager._parse_image(weak_self, image))
+        if notify:
+            self.hud.notification(self.sensors[index][2])
+        self.index = index
+    
+    def next_sensor(self):
+        """Get the next sensor"""
+        self.set_sensor(self.index+1)
+    
+    def toggle_recording(self):
+        """Toggle recording on or off"""
+        self.recording = not self.recording
+        self.hud.notification('Recording %s' % ('On' if self.recording else 'Off'))
+    
+    def render(self, display):
+        """Render method"""
+        if self.surface is not None:
+            display.blit(self.surface, (0,0))
+    
+    @staticmethod
+    def _parse_image(weak_self, image):
+        self = weak_self()
+        if not self:
+            return
+
+        image.convert(self.sensors[self.idnex][1])
+        array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
+        array = np.reshape(array, (image.height, image.width, 4))
+        array = array[:, :, :3]
+        array = array[:, :, ::-1]
+        self.surface = pygame.surfarray.make_surface(array.swapaxes(0, 1))
+
+        if self.recording:
+            image.save_to_disk('_out/%08d' % image.frame)
+    
 ######################## MAIN FUNCTION ###########################
 
-def main(arg):
-    client = carla.Client(arg.host, arg.port)
+def main(args):
+    pygame.init()
+    pygame.font.init()
+    
+    
+    client = carla.Client(args.host, args.port)
     client.set_timeout(10.0)
     world = client.load_world("Town03")
 
     try:    
+        if args.seed:
+            random.seed(args.seed)
+        
         original_settings = world.get_settings()
         settings = world.get_settings()
         traffic_manager = client.get_trafficmanager(8000)
-        traffic_manager.set_synchronous_mode(True)
 
-        delta = 0.05
-        
-        settings.fixed_delta_seconds = delta
-        settings.synchronous_mode = True
-        settings.no_rendering_mode = arg.no_rendering
-        world.apply_settings(settings)
-        
+        if args.sync:
+            delta = 0.05
+            
+            settings.fixed_delta_seconds = delta
+            settings.synchronous_mode = True
+            settings.no_rendering_mode = args.no_rendering
+            world.apply_settings(settings)
+            
+            traffic_manager.set_synchronous_mode(True)
+
+            
         blueprint_library = world.get_blueprint_library()
         vehicle_bp = blueprint_library.filter("model3")[0]
         vehicle_transform = random.choice(world.get_map().get_spawn_points())
         vehicle = world.spawn_actor(vehicle_bp, vehicle_transform)
-        vehicle.set_autopilot(arg.no_autopilot)
-
-        user_offset = carla.Location(arg.x, arg.y, arg.z)
+        vehicle.set_autopilot(args.no_autopilot)
         
+        display = pygame.display.set_mode(
+            (args.width, args.height),
+            pygame.HWSURFACE | pygame.DOUBLEBUF
+        )
+        
+        hud = HUD(args.width, args.height)
+        world = World(client.get_world(), hud, args)
+        controller = KeyboardControl(world)
+
+        clock = pygame.time.Clock()
+
         frame = 0
         dt0 = datetime.now()
 
         while True:
+            clock.tick()
+
             time.sleep(0.005)
             world.tick()
 
@@ -369,11 +491,12 @@ def main(arg):
             frame += 1
 
     finally:
-        world.apply_settings(original_settings)
-        traffic_manager.set_synchronous_mode(False)
-
-        vehicle.destroy()
-        
+        if world is not None:
+            world.apply_settings(original_settings)
+            traffic_manager.set_synchronous_mode(True)
+            world.destroy()
+            
+        pygame.quit()
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(description=__doc__)
