fbd70c818e90d846039f08dbba8af9f74ea6ade3
yoonhero <yoonhero06@naver.com>
2022-02-03 21:39:38 +0900

    feat: Reinforcement AI Snake Agent

--- tech/game.py
+++ tech/game.py
@@ -75,7 +75,7 @@ class SnakeGameAI:
         # 3. check if game over
         reward = 0
         game_over = False
-        if self._is_collision() or self.frame_iteration > 100 * len(self.snake):
+        if self.is_collision() or self.frame_iteration > 100 * len(self.snake):
             game_over = True
             reward = -10
             return reward, game_over, self.score
@@ -94,7 +94,7 @@ class SnakeGameAI:
         # 6. return game over and score
         return reward, game_over, self.score
 
-    def _is_collision(self, pt=None):
+    def is_collision(self, pt=None):
         if pt is None:
             pt = self.head
         # hits boundary
