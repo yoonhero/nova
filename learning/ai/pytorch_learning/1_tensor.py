2c402eda5a9c6c974d13025338338fa727f9bed6
yoonhero <yoonhero06@naver.com>
2022-03-26 00:09:55 +0900

    Pytorch Learning Restart

---
+++ learning/ai/pytorch_learning/1_tensor.py
@@ -0,0 +1,59 @@
+import torch
+import numpy as np
+
+data = [[1,2], [3,4]]
+
+x_data = torch.tensor(data)
+
+np_array = np.array(data)
+x_np = torch.from_numpy(np_array)
+
+x_ones = torch.ones_like(x_data) # x_data의 속성을 유지합니다.
+print(f"Ones Tensor: \n {x_ones} \n")
+
+x_rand = torch.rand_like(x_data, dtype=torch.float) # x_data의 속성을 덮어씁니다.
+print(f"Random Tensor: \n {x_rand} \n")
+
+### Random olr Constant Value ###
+
+shape = (2,3,)
+rand_tensor = torch.rand(shape)
+ones_tensor = torch.ones(shape)
+zeros_tensor = torch.zeros(shape)
+
+print(f"Random Tensor: \n {rand_tensor} \n")
+print(f"Ones Tensor: \n {ones_tensor} \n")
+print(f"Zeros Tensor: \n {zeros_tensor}")
+
+
+### Attribution of Tensor ### 
+
+tensor = torch.rand(3,4)
+
+print(f"Shape of tensor: {tensor.shape}")
+print(f"Datatype of tensor: {tensor.dtype}")
+print(f"Device tensor is stored on: {tensor.device}")
+
+
+### Tensor Operation ###
+if torch.cuda.is_available():
+    tenor = tensor.to("cuda")
+
+tensor = torch.ones(4,4)
+print('First Row: ', tensor[0])
+print('First column: ', tensor[:, 0])
+print("Last Column: ", tensor[..., -1])
+
+tensor[:, 1] = 0
+print(tensor)
+
+### To Numpy ###
+
+t = torch.ones(5)
+print(f"t: {t}")
+n = t.numpy()
+print(f"n: {n}")
+
+t.add_(1)
+print(f"t: {t}")
+print(f"n: {n}")
